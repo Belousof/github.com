41b427fa64e0a85e2f932a630528ad25210c66f0
Belousov A <mpmb.for.reg@gmail.com>
2015-03-30 15:00:53 +0800

    1

---
+++ Templ/Templ/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Templ
+//
+//  Created by Andrey Belousov on 30.03.15.
+//  Copyright (c) 2015 Andrey Belousov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
