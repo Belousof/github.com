5e64fb49949eeadcca2251607a57423c1c517878
Belousov A <mpmb.for.reg@gmail.com>
2015-03-30 15:33:52 +0800

    1

--- Templ/Templ/main.m
+++ Templ/Templ/main.m
@@ -12,10 +12,28 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Перечисления
         enum e{one,two,three};
+        typedef enum {one1 = -9,two1 = 0,three1 = 9} e1;
+        
         enum e x = one;
         enum e y = two;
         enum e z = three;
+        
+        e1 x1 = one1;
+        e1 y1 = two1;
+        e1 z1 = three1;
+        
+        
         NSLog(@"%i - %i - %i",x,y,z);//0 - 1 - 2
+        NSLog(@"[%i] - %i - %i",x1,y1,z1);//-9 - 0 - 9
+        //-----------------------------------------------
+        typedef enum{
+            a = 0,
+            b = 0,
+            c = 1,
+            d = 0
+        } numbers;
+        numbers myPrefOrientire = a;
+        NSLog(@"%i",myPrefOrientire);
     }
     return 0;
 }
